19d16644bf0dc44789195e8e76fb934ded2fdafd
JsphByd <josephboyd113@gmail.com>
2024-05-20 20:24:39 +0000

    Updated installer script

--- Joes_setup.py
+++ Joes_setup.py
@@ -1,13 +1,13 @@
 import os
 
 #need to change the config file to trust the submodule
+os.system("git config --global --add safe.directory /home/ubuntu/MiniNet-Framework/mininet-wifi")
+
 os.system("git submodule init")
+os.system("git submodule update") 
 
-os.system(f"git config --global --add safe.directory /home/ubuntu/MiniNet-Framework/mininet-wifi")
 
-os.system("cd mininet-wifi")
-
-os.system("sudo util/install.sh -Wlnfv")
+os.system("sudo mininet-wifi/util/install.sh -Wlnfv")
 
 os.system("sudo apt install mininet")
 
