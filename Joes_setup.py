cbae7383e2b87bd6062a545c186c61cec0e95bcd
JsphByd <josephboyd113@gmail.com>
2024-05-20 16:23:10 -0600

    Added error messages, fixed installer check script

--- Joes_setup.py
+++ Joes_setup.py
@@ -1,22 +1,40 @@
 import os
 
+RED = "\033[91m"
+GREEN = "\033[92m"
+MAGENTA = "\033[35m"
+CYAN = "\033[36m"
+RESET = "\033[0m"
+
 #need to change the config file to trust the submodule
-os.system("git config --global --add safe.directory $(pwd)")
+supress = ">/dev/null 2>&1"
 
-os.system("echo $(pwd)")
-os.system("git submodule init")
-os.system("git submodule update") 
+print(f"[{GREEN}+{RESET}] Adding Submodules to safe.directory...")
+os.system("git config --global --add safe.directory $(pwd)" + supress)
 
+print(f"[{GREEN}+{RESET}] Initializating Submodules...")
+os.system("git submodule init" + supress)
 
-os.system("sudo mininet-wifi/util/install.sh -Wlnfv")
+os.system("git submodule update" + supress)
 
-os.system("sudo apt install mininet")
+print(f"[{GREEN}+{RESET}] Running Install Script...")
+os.system("sudo mininet-wifi/util/install.sh -Wlnfv" + supress)
+
+print(f"[{GREEN}+{RESET}] Installing Mininet...")
+os.system("sudo apt install -y mininet" + supress)
 
 cwd = os.getcwd() + "/../mininet-wifi"
-print(cwd)
-os.chdir(cwd)
 
-os.system("sudo make install")
+try:
+	os.chdir(cwd)
+except:
+	print(f"[{RED}!{RESET}] Directory Error! Was mininet-wifi successfully deployed?...")
+	exit()
 
-os.system("sudo apt install openvswitch-testcontroller")
-os.system("sudo ln /usr/bin/ovs-testcontroller /usr/bin/controller")
+print(f"[{GREEN}+{RESET}] Compiling...")
+os.system("sudo make install" + supress)
+
+print(f"[{GREEN}+{RESET}] Installing openvswitch-testcontroller...")
+os.system("sudo apt install openvswitch-testcontroller" + supress)
+
+os.system("sudo ln /usr/bin/ovs-testcontroller /usr/bin/controller" + supress)
