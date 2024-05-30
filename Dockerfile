fea7aef8af67f22b32a02db71bc869cfdaf03946
William O <willoldert@gmail.com>
2024-05-30 11:11:12 -0600

    updates to dockerfile and setup.sh

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,15 @@
-FROM ramonfontes/mininet-wifi:latest
+FROM ubuntu:24.04
 
 USER root
 
 WORKDIR /wififorge
 
 COPY . .
-RUN git config --global --add safe.directory $PWD
-RUN git submodule init
-RUN git submodule update
-
-RUN apt-get update -y && apt-get upgrade --fix-missing -y --no-install-recommends
-RUN apt-get install -y --no-install-recommends \
-    curl \
-    aircrack-ng \
-    john \
-    dsniff \
-    mininet --allow-downgrades\
-    openvswitch-testcontroller \
-    openvswitch-switch
-#RUN chmod u+x Framework/dependencies.sh
-#RUN ./Framework/dependencies.sh
-#RUN apt update -y && apt upgrade -y
-RUN ./Framework/mininet-wifi/util/install.sh -Wlnfv
-RUN ln /usr/bin/ovs-testcontroller /usr/bin/controller
-#CMD python3 Framework/WifiForge.py
+ RUN apt-get update -y && apt-get upgrade --fix-missing -y
+ RUN apt install -y \
+    git \
+    sudo \
+    iputils-ping \
+    nano
+#RUN ./Framework/setup.sh
+#CMD service openvswitch-switch 
--- Framework/setup.sh
+++ Framework/setup.sh
@@ -19,43 +19,43 @@ truncated_cwd=$(echo "$cwd" | cut -c 1-"$index")
 
 # Adding Submodules to safe.directory
 echo -e "[${GREEN}+${RESET}] Adding Submodules to safe.directory..."
-git config --global --add safe.directory "$truncated_cwd" > /dev/null 2>&1
+git config --global --add safe.directory "$truncated_cwd" #> /dev/null 2>&1
 
 # Initialize Submodules
 echo -e "[${GREEN}+${RESET}] Initializing Submodules..."
-git submodule init > /dev/null 2>&1
+git submodule init #> /dev/null 2>&1
 
 # Update Submodules
-git submodule update > /dev/null 2>&1
+git submodule update #> /dev/null 2>&1
 
 # Install Kali Tools
 echo -e "[${GREEN}+${RESET}] Installing Kali Tools..."
-sudo apt install curl -y > /dev/null 2>&1
-sudo ./dependencies.sh > /dev/null 2>&1
-sudo apt install aircrack-ng -y > /dev/null 2>&1
-sudo apt install john -y > /dev/null 2>&1
-sudo apt install eaphammer -y > /dev/null 2>&1
-sudo apt install dsniff -y > /dev/null 2>&1
+sudo apt install curl -y #> /dev/null 2>&1
+sudo ./dependencies.sh #> /dev/null 2>&1
+sudo apt install aircrack-ng -y #> /dev/null 2>&1
+sudo apt install john -y #> /dev/null 2>&1
+sudo apt install eaphammer -y #> /dev/null 2>&1
+sudo apt install dsniff -y #> /dev/null 2>&1
 pip install tqdm --break-system-packages
 
 
 # Install Mininet
 echo -e "[${GREEN}+${RESET}] Installing Mininet..."
-sudo apt install mininet -y --allow-downgrades > /dev/null 2>&1
+sudo apt install mininet -y --allow-downgrades #> /dev/null 2>&1
 
 # Move to mininet-wifi directory
 cd "$cwd/mininet-wifi"
 
 # Run Install Script
 echo -e "[${GREEN}+${RESET}] Running Install Script..."
-./util/install.sh -Wlnfv > /dev/null 2>&1
+./util/install.sh -Wlnfv #> /dev/null 2>&1
 
 # Compile
 echo -e "[${GREEN}+${RESET}] Compiling..."
-sudo make install > /dev/null 2>&1
+sudo make install #> /dev/null 2>&1
 
 # Install openvswitch-testcontroller
 echo -e "[${GREEN}+${RESET}] Installing openvswitch-testcontroller..."
-sudo apt install openvswitch-testcontroller -y > /dev/null 2>&1
-sudo ln /usr/bin/ovs-testcontroller /usr/bin/controller > /dev/null 2>&1
+sudo apt install openvswitch-testcontroller -y #> /dev/null 2>&1
+sudo ln /usr/bin/ovs-testcontroller /usr/bin/controller #> /dev/null 2>&1
 
