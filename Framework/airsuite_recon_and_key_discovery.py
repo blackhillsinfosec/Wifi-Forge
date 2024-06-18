dfdc9386d296ab64b410b4dda09311b8f1e0583d
jsphbyd <josephboyd113@gmail.com>
2024-06-18 20:45:26 +0000

    Major updates to files, bug fixes, added menu options

--- Framework/airsuite_recon_and_key_discovery.py
+++ Framework/airsuite_recon_and_key_discovery.py
@@ -57,7 +57,11 @@ def AIRSUITE_RECON_AND_KEY_DISCOVERY():
     net.addLink(host9, ap4)
 
     net.build()
-    ap.start([])
+    ap0.start([])
+    ap1.start([])
+    ap2.start([])
+    ap3.start([])
+    ap4.start([])
     
     print_banner()
     print("\n")
