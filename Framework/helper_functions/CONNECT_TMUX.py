cc637a8c2b876d47446f6437ad0ab36f861cd184
JsphByd <jboyd@blackhillsinfosec.com>
2025-01-15 21:32:25 +0000

    updated naming of tmux panes

--- Framework/helper_functions/CONNECT_TMUX.py
+++ Framework/helper_functions/CONNECT_TMUX.py
@@ -18,22 +18,24 @@ def CONFIG_TMUX(nodes, lab_name):
         session_name = f"WIFIFORGE-{lab_name}"
         session = server.new_session(session_name=session_name, attach=False)
         window = session.windows[0]
-        session.cmd("rename-window", node[0])
         panes = {}
 
-        #Split the window up into panes
+        #Split the window up into panes; 1 pane for each required host 
+        while len(window.panes) != len(nodes):
+            window.panes[len(window.panes) - 1].split(direction=PaneDirection.Left, attach=False)
 
-        for index, node in enumerate(nodes):
-            if index != len(nodes) - 1:
-                window.panes[len(window.panes) - 1].split(direction=PaneDirection.Left, attach=True)
-            process_id = check_output(["ps aux | grep -G 'mininet:"+node+"' | grep -v 'grep' | grep -v 'ap' | awk '{print $2}'"], shell=True).decode("utf-8")
+        #connect and name each pane after the node it's connected to
+        for index, pane, in enumerate(window.panes):
+            process_id = check_output(["ps aux | grep -G 'mininet:"+nodes[index]+"' | grep -v 'grep' | grep -v 'ap' | awk '{print $2}'"], shell=True).decode("utf-8")
             subprocess.Popen(["tmux", "send-keys", "-t", f"{session_name}:0.{index}", f"exec sudo nsenter -t {process_id.rstrip()} -m -u -i -n -p bash", "C-m"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            session.cmd("select-pane", "-T", node)
-        
+            session.cmd("select-pane", "-t", index, "-T", nodes[index])
+
+        #let the user click; make it look nice
         session.cmd("set", "-g", "pane-border-status", "top")
         session.cmd("set", "-g", "mouse", "on")
         print(session.cmd("select-layout", "tiled").stderr)
 
+        #attach to the tmux session
         session.attach()
     except Exception as err:
         print(err)
