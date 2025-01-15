d0bdf9029ab86fe7b71e83a8437fa97322b03965
JsphByd <jboyd@blackhillsinfosec.com>
2025-01-15 18:15:12 +0000

    updated bash commands

--- Framework/helper_functions/CONNECT_TMUX.py
+++ Framework/helper_functions/CONNECT_TMUX.py
@@ -25,16 +25,14 @@ def CONFIG_TMUX(nodes, lab_name):
         for index, node in enumerate(nodes):
             if index != len(nodes) - 1:
                 panes[node] = window.panes[len(window.panes) - 1].split(direction=PaneDirection.Left, attach=False)
-            process_id = check_output(["ps aux | grep -G 'mininet:"+node+"' | grep -v 'grep' | grep -v 'ap1' | awk '{print $2}'"], shell=True).decode("utf-8")
-            subprocess.Popen(["tmux", "send-keys", "-t", f"{session_name}:0.{index}", f"exec sudo nsenter -t {process_id.rstrip()} -m -u -i -n -p bash; clear", "C-m"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-
-
+            process_id = check_output(["ps aux | grep -G 'mininet:"+node+"' | grep -v 'grep' | grep -v 'ap' | awk '{print $2}'"], shell=True).decode("utf-8")
+            subprocess.Popen(["tmux", "send-keys", "-t", f"{session_name}:0.{index}", f"exec sudo nsenter -t {process_id.rstrip()} -m -u -i -n -p bash", "C-m"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         
         print(session.cmd("select-layout", "tiled").stderr)
 
         session.attach()
     except Exception as err:
+        print(err)
         subprocess.Popen(["tmux", "kill-session", "-t", session_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     subprocess.Popen(["tmux", "kill-session", "-t", session_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
