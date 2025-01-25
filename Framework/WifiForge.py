bd795c44167fd862059c09278036a932d3d01ed0
JsphByd <jboyd@blackhillsinfosec.com>
2025-01-25 03:47:06 +0000

    pushed correct main menu file

--- Framework/WifiForge.py
+++ Framework/WifiForge.py
@@ -9,152 +9,139 @@ import inspect
 from mn_wifi.mobility import Mobility, ConfigMobLinks
 from mn_wifi.module import Mac80211Hwsim
 
-# ANSI escape codes for colors
-RED = "\033[91m"
-GREEN = "\033[92m"
-MAGENTA = "\033[35m"
-CYAN = "\033[36m"
-PURPLE = "\033[35m"
-BLUE = "\033[34m"
-YELLOW = "\033[93m"  # Yellow for highlighting
-RESET = "\033[0m"
-BOLD_WHITE = "\033[1;97m"
 
-# BANNER CALL
 def print_banner():
-    os.system("clear")
-    print("""
-    
-                                     ⣀⣤⣶⣿⠷⠾⠛⠛⠛⠛⠷⠶⢶⣶⣤⣄⡀⠀⠀⠀⠀⠀⠀
-                                  ⣀⣴⡾⠛⠉⠁⠀⣰⡶⠶⠶⠶⠶⠶⣶⡄⠀⠉⠛⠿⣷⣄⡀⠀⠀⠀
-                                ⣠⣾⠟⠁⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⣼⠃⠀⠀⠀⠀⠈⠛⢿⣦⡀⠀
-                              ⢠⣼⠟⠁⠀⠀⠀⠀⣠⣴⣶⣿⡇⠀⠀⠀⠀⠀⣿⣷⣦⣄⠀⠀⠀⠀⠀⠙⣧⡀
-                              ⣿⡇⠀⠀⠀⢀⣴⣾⣿⣿⣿⣿⣇⠀⠀⠀⠀⠸⣿⣿⣿⣿⣿⣦⡀⠀⠀⠀⢈⣷
-                              ⣿⣿⣦⡀⣠⣾⣿⣿⣿⡿⠟⢻⣿⠀⠀⠀⠀⢠⣿⠻⢿⣿⣿⣿⣿⣆⣀⣠⣾⣿
-                              ⠉⠻⣿⣿⣿⣿⣽⡿⠋⠀⠀⠸⣿⠀⠀⠀⠀⢸⡿⠀⠀⠉⠻⣿⣿⣿⣿⣿⠟⠁
-                                ⠈⠙⠛⣿⣿⠀⠀⠀⠀⢀⣿⠀⠀⠀⠀⢸⣇⠀⠀⠀⠀⣹⣿⡟⠋⠁⠀⠀
-                                    ⢿⣿⣷⣄⣀⣴⣿⣿⣤⣤⣤⣤⣼⣿⣷⣀⣀⣾⣿⣿⠇⠀⠀⠀⠀
-                                    ⠈⠻⢿⣿⣿⣿⣿⣿⠟⠛⠛⠻⣿⣿⣿⣿⣿⡿⠛⠉⠀⠀⠀⠀⠀
-                                       ⠉⠉⠁⣿⡇⠀⠀⠀⠀⢸⣿⡏⠙⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀
-                                          ⣿⣷⣄⠀⠀⣀⣾⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-                                           ⠙⢿⣿⣿⣿⣿⣿⣏
+	print("  ")
 
-            ██╗       ██╗██╗███████╗██╗  ███████╗ █████╗ ██████╗  ██████╗ ███████╗
-            ██║  ██╗  ██║██║██╔════╝██║  ██╔════╝██╔══██╗██╔══██╗██╔════╝ ██╔════╝
-            ╚██╗████╗██╔╝██║█████╗  ██║  █████╗  ██║  ██║██████╔╝██║  ██╗ █████╗  
-             ████╔═████║ ██║██╔══╝  ██║  ██╔══╝  ██║  ██║██╔══██╗██║  ╚██╗██╔══╝  
-              ██╔╝ ╚██╔╝ ██║██║     ██║  ██║     ╚█████╔╝██║  ██║╚██████╔╝███████╗
-              ╚═╝   ╚═╝  ╚═╝╚═╝     ╚═╝  ╚═╝      ╚════╝ ╚═╝  ╚═╝ ╚═════╝ ╚══════╝
-    """)
+ascii_art = """
+    
+             ⣀⣤⣶⣿⠷⠾⠛⠛⠛⠛⠷⠶⢶⣶⣤⣄⡀⠀⠀⠀⠀⠀⠀
+          ⣀⣴⡾⠛⠉⠁⠀⣰⡶⠶⠶⠶⠶⠶⣶⡄⠀⠉⠛⠿⣷⣄⡀⠀⠀⠀
+        ⣠⣾⠟⠁⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⣼⠃⠀⠀⠀⠀⠈⠛⢿⣦⡀⠀
+     ⢠⣼⠟⠁⠀⠀⠀⠀⣠⣴⣶⣿⡇⠀⠀⠀⠀⠀⣿⣷⣦⣄⠀⠀⠀⠀⠀⠙⣧⡀
+    ⣿⡇⠀⠀⠀⢀⣴⣾⣿⣿⣿⣿⣇⠀⠀⠀⠀⠸⣿⣿⣿⣿⣿⣦⡀⠀⠀⠀⢈⣷
+    ⣿⣿⣦⡀⣠⣾⣿⣿⣿⡿⠟⢻⣿⠀⠀⠀⠀⢠⣿⠻⢿⣿⣿⣿⣿⣆⣀⣠⣾⣿
+    ⠉⠻⣿⣿⣿⣿⣽⡿⠋⠀⠀⠸⣿⠀⠀⠀⠀⢸⡿⠀⠀⠉⠻⣿⣿⣿⣿⣿⠟⠁
+      ⠈⠙⠛⣿⣿⠀⠀⠀⠀⢀⣿⠀⠀⠀⠀⢸⣇⠀⠀⠀⠀⣹⣿⡟⠋⠁⠀⠀
+          ⢿⣿⣷⣄⣀⣴⣿⣿⣤⣤⣤⣤⣼⣿⣷⣀⣀⣾⣿⣿⠇⠀⠀⠀⠀
+          ⠈⠻⢿⣿⣿⣿⣿⣿⠟⠛⠛⠻⣿⣿⣿⣿⣿⡿⠛⠉⠀⠀⠀⠀⠀
+              ⠉⠉⠁⣿⡇⠀⠀⠀⠀⢸⣿⡏⠙⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀
+                  ⣿⣷⣄⠀⠀⣀⣾⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠙⢿⣿⣿⣿⣿⣿⣏
+
+    ██╗       ██╗██╗███████╗██╗  ███████╗ █████╗ ██████╗  ██████╗ ███████╗
+    ██║  ██╗  ██║██║██╔════╝██║  ██╔════╝██╔══██╗██╔══██╗██╔════╝ ██╔════╝
+    ╚██╗████╗██╔╝██║█████╗  ██║  █████╗  ██║  ██║██████╔╝██║  ██╗ █████╗  
+     ████╔═████║ ██║██╔══╝  ██║  ██╔══╝  ██║  ██║██╔══██╗██║  ╚██╗██╔══╝  
+      ██╔╝ ╚██╔╝ ██║██║     ██║  ██║     ╚█████╔╝██║  ██║╚██████╔╝███████╗
+     ╚═╝   ╚═╝  ╚═╝╚═╝     ╚═╝  ╚═╝      ╚════╝ ╚═╝  ╚═╝ ╚═════╝ ╚══════╝
+	 By Black Hills Information Security and Antisyphon
+    """
 
 def remove_old_variables():
-    ConfigMobLinks.aps = []
-    Mac80211Hwsim.hwsim_ids = []
-    os.system("mn -c")
+	ConfigMobLinks.aps = []
+	Mac80211Hwsim.hwsim_ids = []
+	os.system("mn -c")
 
 # Function to import module and get the first function from it
 def import_module_and_get_function(file_path, module_name):
-    spec = importlib.util.spec_from_file_location(module_name, file_path)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    
-    # Get the first function defined in the module, ignoring 'print_banner'
-    functions = {name: func for name, func in inspect.getmembers(module, inspect.isfunction) if name != 'print_banner' and name != "CONFIG_TMUX"}
-    if functions:
-        function_name, function = next(iter(functions.items()))
-        return function_name, function
-    return None, None
+	spec = importlib.util.spec_from_file_location(module_name, file_path)
+	module = importlib.util.module_from_spec(spec)
+	spec.loader.exec_module(module)
+	
+	# Get the first function defined in the module, ignoring 'print_banner'
+	functions = {name: func for name, func in inspect.getmembers(module, inspect.isfunction) if name != 'print_banner' and name != "CONFIG_TMUX"}
+	if functions:
+		function_name, function = next(iter(functions.items()))
+		return function_name, function
+	return None, None
 
 # Function to read the names of .py files in a directory, import the first function from each, and allow calling them
 def load_functions_from_py_files(directory):
-    function_dict = {}
-    current_script = os.path.basename(__file__)
-    for filename in os.listdir(directory):
-        if filename.endswith(".py") and not filename.startswith(".") and filename != current_script:
-            module_name = filename[:-3]  # Remove the .py extension
-            file_path = os.path.join(directory, filename)
-            function_name, function = import_module_and_get_function(file_path, module_name)
-            if function_name and function:
-                function_dict[filename] = (function_name, function)
-    return function_dict
+	function_dict = {}
+	current_script = os.path.basename(__file__)
+	for filename in os.listdir(directory):
+		if filename.endswith(".py") and not filename.startswith(".") and filename != current_script:
+			module_name = filename[:-3]  # Remove the .py extension
+			file_path = os.path.join(directory, filename)
+			function_name, function = import_module_and_get_function(file_path, module_name)
+			if function_name and function:
+				function_dict[filename] = (function_name, function)
+	return function_dict
+
+class suspend_curses():
+    """Context Manager to temporarily leave curses mode"""
+
+    def __enter__(self):
+        curses.endwin()
+
+    def __exit__(self, exc_type, exc_val, tb):
+        newscr = curses.initscr()
+        newscr.addstr('Newscreen is %s\n' % newscr)
+        newscr.refresh()
+        curses.doupdate()
 
 # Ensure this variable is populated before it's used
 directory = os.path.dirname(os.path.realpath(__file__)) + "/labs"  # Current directory
 functions = load_functions_from_py_files(directory)
+file_names = []
+menu = []
 
-print(functions)
+for file in functions.keys():
+	formatted_name = file.replace("_", ' ').title()[:-3]
+	file_names.append(file)
+	menu.append(formatted_name)
 
-class Menu(object):
-    def __init__(self, items, stdscreen):
-        self.window = stdscreen.subwin(0, 0)
-        self.window.keypad(1)
-        self.panel = panel.new_panel(self.window)
-        self.panel.hide()
-        panel.update_panels()
+def print_menu(stdscr, selected_row_index):
+	h, w = stdscr.getmaxyx()
+	stdscr.clear()
+    #print the banner
 
-        self.position = 0
-        self.items = items
-        self.items.append(("exit", "exit"))
+	for index, line in enumerate(ascii_art.splitlines(), 2):
+		x = w//2 - len(line)//2
+		y = h//3 - len(ascii_art.splitlines())//2 + index
+		stdscr.addstr(y, x, line)
+	
+	for index, row in enumerate(menu):
+		x = w//2 - len(row)//2
+		y = h//2 - (len(menu) - len(ascii_art.splitlines()))//2 + index
+		if index == selected_row_index:
+			stdscr.attron(curses.color_pair(1))
+			stdscr.addstr(y, x, row)
+			stdscr.attroff(curses.color_pair(1))
+		else:
+			stdscr.addstr(y, x, row)
 
-    def navigate(self, n):
-        self.position += n
-        if self.position < 0:
-            self.position = 0
-        elif self.position >= len(self.items):
-            self.position = len(self.items) - 1
+	stdscr.refresh()
 
-    def display(self):
-        self.panel.top()
-        self.panel.show()
-        self.window.clear()
+def main(stdscr):
+	# turn off cursor blinking
+	curses.curs_set(0)
 
-        while True:
-            self.window.refresh()
-            curses.doupdate()
-            for index, item in enumerate(self.items):
-                if index == self.position:
-                    mode = curses.A_REVERSE
-                else:
-                    mode = curses.A_NORMAL
+	# color scheme for selected row
+	curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
 
-                msg = "%d. %s" % (index, item[0])
-                self.window.addstr(1 + index, 1, msg, mode)
+	# specify the current selected row
+	current_row = 0
 
-            key = self.window.getch()
-
-            if key in [curses.KEY_ENTER, ord("\n")]:
-                if self.position == len(self.items) - 1:
-                    break
-                else:
-                    self.items[self.position][1]()
-
-            elif key == curses.KEY_UP:
-                self.navigate(-1)
-
-            elif key == curses.KEY_DOWN:
-                self.navigate(1)
-
-        self.window.clear()
-        self.panel.hide()
-        panel.update_panels()
-        curses.doupdate()
-
-class MyApp(object):
-    def __init__(self, stdscreen):
-        self.screen = stdscreen
-        curses.curs_set(0)
-
-        test = [("HELLO", curses.beep), ("TEST", curses.flash)]
-        main_menu = []
-
-        for item in test:
-            main_menu.append(item)
-
-        print(main_menu)
-        main_menu = Menu(main_menu, self.screen)
-        main_menu.display()
+	print_menu(stdscr, current_row)
 
 
+	while True:
+		key = stdscr.getch()
+		if key == curses.KEY_UP and current_row > 0:
+			current_row -= 1
+		elif key == curses.KEY_DOWN and current_row < len(menu)-1:
+			current_row += 1
+		elif key == curses.KEY_ENTER or key in [10, 13]:
+			with suspend_curses():
+				func = functions[file_names[current_row]][1]
+				func()
+				remove_old_variables()
+	
+		print_menu(stdscr, current_row)
+
+		
 
 if __name__ == "__main__":
-    curses.wrapper(MyApp)
+	curses.wrapper(main)
