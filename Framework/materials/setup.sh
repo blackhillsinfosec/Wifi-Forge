9c114793a4ce8a13da2882ec31f4366b54f15d43
Joe B. <95513994+JsphByd@users.noreply.github.com>
2025-03-06 15:44:35 -0700

    Update setup.sh

--- Framework/materials/setup.sh
+++ Framework/materials/setup.sh
@@ -19,51 +19,49 @@ truncated_cwd=$(echo "$cwd" | cut -c 1-"$index")
 
 # Adding Submodules to safe.directory
 echo -e "[${GREEN}+${RESET}] Adding Submodules to safe.directory..."
-git config --global --add safe.directory "$truncated_cwd" #> /dev/null 2>&1
+git config --global --add safe.directory "$truncated_cwd" > /dev/null 2>&1
 
 # Initialize Submodules
 echo -e "[${GREEN}+${RESET}] Initializing Submodules..."
-git submodule init #> /dev/null 2>&1
+git submodule init > /dev/null 2>&1
 
 # Update Submodules
-git submodule update #> /dev/null 2>&1
+git submodule update > /dev/null 2>&1
 
 #set global pip variable break system packages to true
-sudo python3 -m pip config set global.break-system-packages true
+sudo python3 -m pip config set global.break-system-packages true  > /dev/null 2>&1
 
 
 # Install Kali Tools
-echo -e "[${GREEN}+${RESET}] Installing Kali Tools..."
-sudo apt install curl -y #> /dev/null 2>&1
-sudo ./dependencies.sh #> /dev/null 2>&1
-sudo apt install aircrack-ng -y #> /dev/null 2>&1
-sudo apt install john -y #> /dev/null 2>&1
-sudo apt install eaphammer -y #> /dev/null 2>&1
-sudo apt install dsniff -y #> /dev/null 2>&1
-sudo apt install tmux -y #> /dev/null 2>&1
-sudo pip install tqdm
-sudo pip install keyboard
-sudo pip install libtmux
+echo -e "[${GREEN}+${RESET}] Installing Tools..."
+sudo apt update -y > /dev/null 2>&1
+sudo apt install pip -y > /dev/null 2>&1
+sudo apt install curl -y > /dev/null 2>&1
+sudo apt install aircrack-ng -y > /dev/null 2>&1
+sudo apt install john -y > /dev/null 2>&1
+sudo apt install dsniff -y > /dev/null 2>&1
+sudo apt install tmux -y > /dev/null 2>&1
+sudo pip install tqdm --break-system-packages > /dev/null 2>&1
+sudo pip install keyboard --break-system-packages > /dev/null 2>&1
+sudo pip install libtmux --break-system-packages > /dev/null 2>&1
 
 
 # Install Mininet
 echo -e "[${GREEN}+${RESET}] Installing Mininet..."
-sudo apt install mininet -y --allow-downgrades #> /dev/null 2>&1
+sudo apt install mininet -y --allow-downgrades > /dev/null 2>&1
 
-# Move to mininet-wifi directory
-cd "$cwd/mininet-wifi"
 
 # Run Install Script
 echo -e "[${GREEN}+${RESET}] Running Install Script..."
-../mininet-wifi/util/install.sh -Wlnf #> /dev/null 2>&1
+../mininet-wifi/util/install.sh -Wlnf > /dev/null 2>&1
 
 # Compile
 echo -e "[${GREEN}+${RESET}] Compiling..."
-sudo make install #> /dev/null 2>&1
+sudo make install > /dev/null 2>&1
 
 # Install openvswitch-testcontroller
 echo -e "[${GREEN}+${RESET}] Installing openvswitch-testcontroller..."
-sudo apt install openvswitch-testcontroller -y #> /dev/null 2>&1
-sudo ln /usr/bin/ovs-testcontroller /usr/bin/controller #> /dev/null 2>&1
-sudo service openvswitch-switch start
-sudo cp ./main_menu /bin
+sudo apt install openvswitch-testcontroller -y > /dev/null 2>&1
+sudo ln /usr/bin/ovs-testcontroller /usr/bin/controller > /dev/null 2>&1
+sudo service openvswitch-switch start  > /dev/null 2>&1
+sudo cp ./main_menu /bin  > /dev/null 2>&1
