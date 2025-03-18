c8662122ab9484a8a76887d83175398a9c2c8e03
The Heretic <97698397+her3ticAVI@users.noreply.github.com>
2025-03-17 23:17:07 -0600

    Update bettercap_wifi_auth_capture.py

--- Framework/labs/bettercap_wifi_auth_capture.py
+++ Framework/labs/bettercap_wifi_auth_capture.py
@@ -8,7 +8,7 @@ def BETTERCAP_WIFI_AUTH_CAPTURE():
     net = Mininet_wifi()
 
     print('Creating Stations')
-    attacker = net.addStation('a', wlans=1)
+    attacker = net.addStation('Attacker', wlans=1)
 
     #secure_wifi
     host1 = net.addStation('host1', passwd='123456789a', encrypt='wep')
